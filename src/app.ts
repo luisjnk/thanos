f9c57f7c5b61b60945831dc291290162ac571ff1
luis <luisjnk@gmail.com>
2019-03-23 12:13:10 -0300

    added new packages into project

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,21 @@
-console.log('hello world')
+import * as express from 'express'
 
-let a : string  = 'hello'
+class App  {
+
+    public express : express.Application;
+
+    constructor() {
+        this.express = express();
+        this.middleware()
+    }
+
+    private middleware() : void {
+        this.express.use('/hello', (req : express.Request, res: express.Response, next: express.NextFunction) => {
+            res.send({
+                hello : 'hello world!'
+            })
+        })
+    }
+}
+
+export default new App().express;
